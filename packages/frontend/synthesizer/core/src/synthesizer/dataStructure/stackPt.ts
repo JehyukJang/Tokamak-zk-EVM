4a0e60d220b8f0cf216188e0df41ec6ef26945c9
Jehyuk <cjhyuck213@gmail.com>
2026-08-12 10:43:37 +0800

    chore(synthesizer): remove unused stack exchange

--- packages/frontend/synthesizer/core/src/synthesizer/dataStructure/stackPt.ts
+++ packages/frontend/synthesizer/core/src/synthesizer/dataStructure/stackPt.ts
@@ -182,26 +182,6 @@ export class StackPt {
   }
 
   /**
-   * Swap number 1 with number 2 on the stack
-   * @param swap1
-   * @param swap2
-   */
-  exchange(swap1: number, swap2: number) {
-    const headIndex = this._len - 1
-    const exchangeIndex1 = headIndex - swap1
-    const exchangeIndex2 = headIndex - swap2
-
-    // Stack underflow is not possible in EOF
-    if (exchangeIndex1 < 0 || exchangeIndex2 < 0) {
-      throw new Error('Unreachable stackPt index')
-    }
-
-    const cache = this._storePt[exchangeIndex2]
-    this._storePt[exchangeIndex2] = this._storePt[exchangeIndex1]
-    this._storePt[exchangeIndex1] = cache
-  }
-
-  /**
    * Returns a copy of the current stack. This represents the actual state of the stack
    * (not the internal state of the stack, which might have unreachable elements in it)
    */
