1b3256cbc9791afe2777584b71c6dee12678c5ec
Jehyuk <cjhyuck213@gmail.com>
2026-08-13 19:36:38 +0800

    refactor(synthesizer): colocate log cache with state manager

--- packages/frontend/synthesizer/core/src/synthesizer/handlers/index.ts
+++ packages/frontend/synthesizer/core/src/synthesizer/handlers/index.ts
@@ -1,6 +1,5 @@
 export * from './subcircuitOutputCalculator.ts'
 export * from './bufferManager.ts'
 export * from './stateManager.ts'
-export * from './logAccess.ts'
 export * from './storageAccess.ts'
 export * from './instructionHandler.ts'
