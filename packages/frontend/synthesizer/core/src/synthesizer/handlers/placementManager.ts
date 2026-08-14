b25064ea40161c932d6a70191822751ec6fc8e21
Jehyuk <cjhyuck213@gmail.com>
2026-08-14 11:01:11 +0800

    refactor(synthesizer): distinguish Circom and EVM zero constants

--- packages/frontend/synthesizer/core/src/synthesizer/handlers/placementManager.ts
+++ packages/frontend/synthesizer/core/src/synthesizer/handlers/placementManager.ts
@@ -379,6 +379,7 @@ export class PlacementManager {
 
     this.addReservedVariableToBufferIn('CIRCOM_CONST_ONE', 1n)
     this.addReservedVariableToBufferIn('CIRCOM_CONST_ZERO', 0n)
+    this.addReservedVariableToBufferIn('EVM_CONST_ZERO', 0n)
     this.addReservedVariableToBufferIn('ADDRESS_MASK', (1n << 160n) - 1n)
     this.addReservedVariableToBufferIn('BYTE_MASK', 0xffn)
     this.addReservedVariableToBufferIn('JUBJUB_BASE_X', jubjub.Point.BASE.toAffine().x)
